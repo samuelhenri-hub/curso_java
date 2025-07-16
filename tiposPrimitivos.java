2f6ad1073561db1585c5a9c91438135150a356eb
Samuel <henrisamuel234@gmail.com>
2025-07-16 18:02:27 -0300

    double

--- tiposPrimitivos.java
+++ tiposPrimitivos.java
@@ -2,9 +2,7 @@ public class tiposPrimitivos {
     public static void main(String[] args) {
         
         String nome = "Samuel";
-
         int idade = 19;
-
-        
+        double altura = 1.76;
     }
 }
