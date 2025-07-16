4fcae3bdc9f5524dea72102893021fd8357580f5
Samuel <henrisamuel234@gmail.com>
2025-07-16 18:08:03 -0300

    pronto

--- tiposPrimitivos.java
+++ tiposPrimitivos.java
@@ -5,5 +5,8 @@ public class tiposPrimitivos {
         int idade = 19;
         double altura = 1.76;
         boolean faculdade = true;
+
+        System.out.println("Meu nome é" + nome + "e eu tenho" + idade + "anos" + "tenho"
+        + altura + "de altura" + "e faço faculdade" + faculdade);
     }
 }
